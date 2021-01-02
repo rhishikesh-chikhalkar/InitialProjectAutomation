b24048d387f72e484c2a9cf80e63932fefb692e6
Rhishikesh <rhishichikhalkar21@gmail.com>
2021-01-02 19:38:39 +0530

    Working Jan 2021

--- windows_OS/local.py
+++ windows_OS/local.py
@@ -1,8 +1,8 @@
 import sys
 import os
 
-os.environ['mp'] = 'D:\CodeWithHarryPrograms\GitHub'
-os.environ['gt'] = '095b5f26db73a48cc63aef20f08b07b8f4f91e6f'
+os.environ['mp'] = 'D:\Programs\GitHub'
+os.environ['gt'] = '6534b22c879956aa28e6c2ad055a4b5c7de3e7b0'
 
 foldername = str(sys.argv[1])
 path = os.environ.get('mp')
